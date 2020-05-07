39d5470cf7940f5f5e0d0c83ead8e63824f3371f
Jonas Landt <landt@sorthvid.net>
2020-05-07 11:00:16 +0200

    Wishlist started

---
+++ components/content/WishList.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="wish-list">
+		WISHLIST LENGTH: {{wishList.length}}
+	</div>
+</template>
+
+<script>
+
+import { vuex, mapActions, mapState } from 'vuex'
+
+// import CollectionItem from '~/components/content/CollectionItem.vue'
+
+export default {
+	name:'wish-list',
+	components: {
+		// CollectionItem
+	},
+	computed: {
+		...mapState({
+			wishList: state => state.collection.wishList
+		})
+	}
+};
+
+</script>
