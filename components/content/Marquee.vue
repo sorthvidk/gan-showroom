dfd47190f28d8502a1fa5ba8560481b24f2817fe
erikthalen <erik.thalen@hyperisland.se>
2020-05-11 15:27:34 +0200

    marquee - the marquee works

--- components/content/Marquee.vue
+++ components/content/Marquee.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="marquee">
+	<div class="marquee marquee3k" data-speed="1" data-pausable="true">
 		<p @click="marqueeClickHandler">{{text}}</p>
 	</div>
 </template>
 
 <script>
-
 import { vuex, mapActions, mapState } from 'vuex'
 import { OPEN_STYLE_CONTENT } from '~/model/constants'
 import getCloudinaryUrl from '~/utils/cloudinary-url'
+import Marquee3k from 'marquee3000'
 
 export default {
-	name:'marquee',
+	name: 'marquee',
 	props: {
 		text: {
 			type: String,
@@ -19,12 +19,13 @@ export default {
 		}
 	},
 	methods: {
-		...mapActions([
-			OPEN_STYLE_CONTENT.action
-		]),	
+		...mapActions([OPEN_STYLE_CONTENT.action]),
 		marqueeClickHandler() {
-			this[OPEN_STYLE_CONTENT.action]('F5987334');
+			this[OPEN_STYLE_CONTENT.action]('F5987334')
 		}
+	},
+	mounted() {
+		Marquee3k.init()
 	}
-};
+}
 </script>
