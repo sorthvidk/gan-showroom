263a4aac13e467b21ade9159ec09e476b599798e
erikthalen <erik.thalen@hyperisland.se>
2020-05-19 15:54:47 +0200

    login - added slides and password

--- plugins/vue-plugins.js
+++ plugins/vue-plugins.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import VueDraggableResizable from 'vue-draggable-resizable'
+import VueTyper from 'vue-typer'
 
 Vue.use(VueDraggableResizable)
+Vue.use(VueTyper)
 
 import cloudinary from 'cloudinary-core'
 
-const $cloudinary = new cloudinary.Cloudinary({cloud_name: 'dd6fpxydm', secure: true})
-$cloudinary.init();
+const $cloudinary = new cloudinary.Cloudinary({
+	cloud_name: 'dd6fpxydm',
+	secure: true
+})
+$cloudinary.init()
 Vue.prototype.$cloudinary = $cloudinary
 
-
 export default ({ app }, inject) => {
-  inject('cloudinary', $cloudinary)
+	inject('cloudinary', $cloudinary)
 }
