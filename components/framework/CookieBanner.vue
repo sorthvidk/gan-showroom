5284f174c5650e675e78496952d1e0388a718304
Jonas Landt <landt@sorthvid.net>
2020-06-09 10:59:51 +0200

    Ganni Girls hidden, cookie banner css

--- components/framework/CookieBanner.vue
+++ components/framework/CookieBanner.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="cookie-banner">
 		<div>			
-			<h3>GANNI USES COOKIES ON THIS WEBSITE</h3>
+			<h3>GANNI SPACE uses cookies</h3>
 			<p>We use cookies to analyse how our website is used and to simplify and improve your visit. If you continue to browse our website, you accept our use of cookies, in accordance with our <a href="/cookies" target="_blank">Cookie Policy</a>.</p>
 		</div>
 		
-		<button class="button close" @click="closeHandler">Close</button>
+		<button class="button accept" @click="acceptHandler">Accept all</button>
 	</div>
 </template>
 
@@ -23,7 +23,7 @@ export default {
 		...mapActions([
 			COOKIES_ACCEPT.action
 		]),
-		closeHandler() {
+		acceptHandler() {
 			this[COOKIES_ACCEPT.action]();
 		}
 	}
