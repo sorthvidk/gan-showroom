1bda563a00354e3b9bf1cf742b26938f82da8a03
Erik Thalén <erik@sorthvid.net>
2021-01-14 18:04:07 +0100

    Added test suite

---
+++ test/config.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const ASSET_FOLDER = path.join(__dirname, `../assets/test/`)
+
+module.exports = {
+	COLLECTION_ITEMS_PATH: ASSET_FOLDER + 'collectionItems'
+}
---
+++ test/helpers.js
@@ -0,0 +1,25 @@
+const glob = require('glob')
+const fs = require('fs')
+
+const getPathToEveryJSONInDirectory = folder => cb =>
+	glob(`${folder}/*.json`, (_, paths) => paths.forEach(path => cb(path)))
+
+/**
+ * returns a callback that will be invoked on every JSON in a directory
+ * callback will have ({ filePath, data }) as arguments
+ *
+ * this will log every file's abs path:
+ *
+ * const files = applyCBToEveryJSONInDirectory(./src)
+ * files(({ filePath }) => console.log(filePath))
+ */
+const getEveryJSONInDirectory = folder => cb =>
+	getPathToEveryJSONInDirectory(folder)(filePath =>
+		fs.readFile(filePath, 'utf8', (_, data) =>
+			cb({ filePath, data: JSON.parse(data) })
+		)
+	)
+
+module.exports = {
+	getEveryJSONInDirectory
+}
