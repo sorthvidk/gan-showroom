b2f4686c55adae1363898ea8ea7d55a22589be66
Erik Thalén <erik@sorthvid.net>
2021-01-15 14:53:31 +0100

    test - added more tests

--- test/config.js
+++ test/config.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
-const ASSET_FOLDER = path.join(__dirname, `../assets/test/`)
+const BASE_PATH = path.join(__dirname, '../')
+const ASSET_FOLDER = path.join(BASE_PATH, `assets/test/`)
 
 module.exports = {
+	NETLIFY_CMS_CONFIG_FILE_PATH: BASE_PATH + 'static/admin/config.yml',
 	COLLECTION_ITEMS_PATH: ASSET_FOLDER + 'collectionItems'
 }
--- test/helpers.js
+++ test/helpers.js
@@ -1,25 +1,72 @@
 const glob = require('glob')
 const fs = require('fs')
+const yaml = require('js-yaml')
 
-const getPathToEveryJSONInDirectory = folder => cb =>
-	glob(`${folder}/*.json`, (_, paths) => paths.forEach(path => cb(path)))
+/**
+ * returns an array of strings
+ */
+const getPathToEveryJSONInDirectory = folder =>
+	new Promise((resolve, reject) => {
+		glob(`${folder}/*.json`, (err, paths) => {
+			if (err) {
+				console.warn(err)
+				reject(err)
+			} else {
+				resolve(paths)
+			}
+		})
+	})
+
+const readFile = filePath =>
+	new Promise((resolve, reject) =>
+		fs.readFile(filePath, 'utf8', (err, data) => {
+			if (err) {
+				console.warn(err)
+				reject(err)
+			} else {
+				resolve({ filePath, data: JSON.parse(data) })
+			}
+		})
+	)
 
 /**
  * returns a callback that will be invoked on every JSON in a directory
  * callback will have ({ filePath, data }) as arguments
  *
- * this will log every file's abs path:
+ * this will log abs path of each json file in /src:
  *
  * const files = getEveryJSONInDirectory(./src)
  * files(({ filePath }) => console.log(filePath))
  */
-const getEveryJSONInDirectory = folder => cb =>
-	getPathToEveryJSONInDirectory(folder)(filePath =>
-		fs.readFile(filePath, 'utf8', (_, data) =>
-			cb({ filePath, data: JSON.parse(data) })
+const getEveryJSONInDirectory = folder =>
+	new Promise((resolve, reject) =>
+		getPathToEveryJSONInDirectory(folder).then(paths =>
+			Promise.all(paths.map(path => readFile(path))).then(files =>
+				resolve(files)
+			)
 		)
 	)
 
+/**
+ * prints my-file.yml in json format:
+ *
+ * const json = getYMLFileAsJSON(my-file.yml)
+ * json(file => console.log(file))
+ */
+const getYMLFileAsJSON = filePath => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filePath, 'utf8', (err, ymlFile) => {
+			if (err) {
+				console.warn(err)
+				reject(err)
+			} else {
+				resolve(yaml.load(ymlFile))
+			}
+		})
+	})
+}
+
 module.exports = {
-	getEveryJSONInDirectory
+	getEveryJSONInDirectory,
+	getYMLFileAsJSON
 }
