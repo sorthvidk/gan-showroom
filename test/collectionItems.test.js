ac9768f172753b2b71fc77563ff3f2cc3c23e5ec
Erik Thalén <erik@sorthvid.net>
2021-01-15 09:28:11 +0100

    test - upd workflows

--- test/collectionItems.test.js
+++ test/collectionItems.test.js
@@ -2,21 +2,25 @@ const path = require('path')
 const config = require('./config.js')
 const helpers = require('./helpers.js')
 
-const files = helpers.getEveryJSONInDirectory(config.COLLECTION_ITEMS_PATH)
+const eachCollectionItem = helpers.getEveryJSONInDirectory(
+	config.COLLECTION_ITEMS_PATH
+)
 
 describe('Collection Items', () => {
 	test('Each file should be a .json', () => {
-		files(({ filePath }) => expect(path.extname(filePath)).toBe('.json'))
+		eachCollectionItem(({ filePath }) =>
+			expect(path.extname(filePath)).toBe('.json')
+		)
 	})
 
 	test('groupId should not include spaces', () => {
-		files(({ data }) => expect(data.groupId).not.toMatch(/ /))
+		eachCollectionItem(({ data }) => expect(data.groupId).not.toMatch(/ /))
 	})
 
 	test('styleId should be uniqe', () => {
 		const styleIds = []
-		files(({ data }) => {
-			expect(styleIds.includes(data.styleId)).toBe(false)
+		eachCollectionItem(({ data }) => {
+			expect(styleIds).not.toContain(data.styleId)
 			styleIds.push(data.styleId)
 		})
 	})
