f16a7caddedca2fda3e48941dc5bbc5b09821d6c
Erik Thalén <erik@sorthvid.net>
2021-05-17 11:14:37 +0200

    updated prices for collectionItems

---
+++ bulk/updateBulkValue.mjs
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import glob from 'glob'
+
+const data = fs.readFileSync('./files/21.4-bulk-upload-NEW-GBP-PRICES.json')
+const json = JSON.parse(data)
+
+const assetsFolder = '../assets/content/collectionItems'
+
+json
+	.map(f => ({ ...f, retailPriceGBP: parseInt(f.retailPriceGBP) }))
+	.forEach(updatedPrice => {
+		glob(`${assetsFolder}/*${updatedPrice.styleId}*.json`, (er, [file]) => {
+			if (!file) return
+
+			const oldJson = JSON.parse(fs.readFileSync(file, 'utf8'))
+
+			const newContent = {
+				...oldJson,
+				...updatedPrice
+			}
+
+			// console.log(newContent)
+
+			fs.writeFile(file, JSON.stringify(newContent), err => {
+				if (err) return console.log(err)
+			})
+		})
+	})
